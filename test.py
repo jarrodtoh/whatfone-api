1943197c202841915a59ec501e0d3c6245beac64
Jarrod Toh <jarrod.toh@gmail.com>
2013-10-24 22:16:40 +0800

    Tagging Stage 2 done. Trained Stage 3 too

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 import nltk
+from libraries import files
 
-s = 'this is a sentence.. please tokenize it. thanks!'
+reviews = files.read_from_xml('data/3/corrected3.xml')
+count = 0
+for review in reviews:
+    text = review.content.split()
+    for token in text:
+        str_token = nltk.str2tuple(token, '/')
+        if str_token[1] is None or str_token[1] == '':
+            print str_token
+            count += 1
 
-ss = nltk.word_tokenize(s)
-print nltk.pos_tag(ss)
+print count
