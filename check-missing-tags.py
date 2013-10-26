f5a201eb7913a91d01375bf78793dd15699382ec
Jarrod Toh <jarrod.toh@gmail.com>
2013-10-26 15:00:32 +0800

    New API call for checking for missing tag. Stage 3 done, Stage 4 ongoing

---
+++ check-missing-tags.py
@@ -0,0 +1,20 @@
+import sys
+import nltk
+from libraries import files
+
+if len(sys.argv) >= 2:
+	FILE = sys.argv[1]
+	reviews = files.read_from_xml(FILE)
+	count = 0
+	for review in reviews:
+		text = review.content.split()
+		for token in text:
+			str_token = nltk.str2tuple(token, '/')
+			if str_token[1] is None or str_token[1] == '':
+				print str_token
+				count += 1
+
+	print count
+else:
+	print 'Invalid command.Please use the format:\n python check-missing-tags.py <filename>'
+	sys.exit()
